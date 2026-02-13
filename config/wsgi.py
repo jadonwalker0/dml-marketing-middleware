6d074287843839abd71b10ade9cd5cee9c4ff0cd
Jadon Walker <jwalker@directmortgageloans.com>
2026-02-12 18:46:27 -0600

    add config/wsgi.py logging

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,10 +8,18 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 # path to this file: "dml-marketing-middleware/config/>file<"
 
+import logging
 import os
 from django.core.wsgi import get_wsgi_application
+from django.db import connection
 
 # Force the correct settings module no matter what Azure/env is doing
 os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings"
 
 application = get_wsgi_application()
+
+logger = logging.getLogger(__name__)
+logger.warning("WEB DB: %s %s %s",
+               connection.vendor,
+               connection.settings_dict.get("HOST"),
+               connection.settings_dict.get("NAME"))
