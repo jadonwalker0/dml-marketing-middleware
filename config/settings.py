0a4783c93924d309172f96f3704282e369251f69
Jadon Walker <jwalker@directmortgageloans.com>
2026-02-12 20:38:43 -0600

    fu

--- config/settings.py
+++ config/settings.py
@@ -8,12 +8,14 @@ REMOVE the middleware after successful login!
 # path to this file: "dml-marketing-middleware/config/settings.py"
 import os
 import re
+import pymysql
 from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+pymysql.install_as_MySQLdb()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
